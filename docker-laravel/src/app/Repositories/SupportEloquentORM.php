3ebb75d6929dc43bdb1db1454b79908943f17ee6
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-10-01 16:51:37 -0300

    Presenter

--- docker-laravel/src/app/Repositories/SupportEloquentORM.php
+++ docker-laravel/src/app/Repositories/SupportEloquentORM.php
@@ -6,12 +6,27 @@ use App\DTO\CreateSupportDTO;
 use App\DTO\UpdateSupportDTO;
 use App\Models\Support;
 use App\Repositories\SupportRepositoryinterface;
+use App\Repositories\PaginationInterface;
 use stdClass;
 
 class SupportEloquentORM implements SupportRepositoryinterface {
 
     public function __construct(protected Support $model){
 
+    }
+
+    public function paginate(int $page=1, int $totalPerPage=15, string $filter=null): PaginationInterface{
+
+        $result = $this->model
+                    ->where(function($query) use ($filter){
+                        if ($filter) {
+                            $query->where('subject',$filter);
+                            $query->orWhere('body','like',"%{$filter}%");
+                        }
+                    })
+                    ->paginate($totalPerPage, ['*'], 'page', $page);
+                    
+        return new PaginationPresenter($result);
     }
 
     public function getAll(string $filter=null): array{
--- docker-laravel/src/app/Repositories/SupportRepositoryinterface.php
+++ docker-laravel/src/app/Repositories/SupportRepositoryinterface.php
@@ -7,9 +7,12 @@ use App\DTO\UpdateSupportDTO;
 use stdClass;
 
 interface SupportRepositoryinterface{
+
+    public function paginate(int $page=1, int $totalPerPage=15, string $filter=null): PaginationInterface;
     public function getAll(string $filter=null): array;
     public function findOne(string $id): stdClass|null;
     public function delete(string $id): void;
     public function new(CreateSupportDTO $dto): stdClass;
     public function update(UpdateSupportDTO $dto): stdClass|null;
+
 }
